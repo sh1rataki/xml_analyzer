7e069e8a2016cf68717a2ce9b4e6a08cca2a0197
Hikaru <hikaru_iwasa@chiba-u.jp>
2020-07-14 20:19:05 +0900

    できた

--- src/main.py
+++ src/main.py
@@ -1,17 +1,37 @@
 import xml.etree.ElementTree as ET
+import re
+import urllib.request
+import numpy as np 
+import pprint
 
-def readXML(filename):
-    tree = ET.parse(filename)
-    return tree.getroot()
+def analyzeSitemap(root):
+    
+    sitemaps = list()
+    
+    # remove xml namespace
+    rootTag = re.sub(r'{.+}',"",root.tag)
+    
+    # initial file
+    if rootTag == "sitemapindex":
+        for child in root:
+            sitemapURL = child[0].text
+            response = urllib.request.urlopen(sitemapURL)
+            sitemap = response.read().decode()
+            sitemapRoot = ET.fromstring(sitemap)
+            sitemaps.append(analyzeSitemap(sitemapRoot))
+    # sitemap child file 
+    elif rootTag == "urlset":
+        for child in root:
+            sitemaps.append(child[0].text)
+            # print(child[0].text)
+    return sitemaps
 
-
+# readSItemapIndex
 def main(filename):
-    root = readXML(filename)
-    print(root.tag)
-    # if root.tag = ""
-    # for child in root:
-        # print(child[0].text)
- 
+    sitemapIndexTree = ET.parse(filename)
+    root = sitemapIndexTree.getroot()
+    urls =analyzeSitemap(root)
+    pprint.pprint(urls)
 
 
 main("../XML Sitemap.xml")
